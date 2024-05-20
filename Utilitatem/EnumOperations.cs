1f19e73f4a2e2e726ada163f87dd8b36cae5a5e4
Bob McAllan <bobmcallan@gmail.com>
2024-05-20 13:37:13 +1000

    Updated

---
+++ Utilitatem/EnumOperations.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace NasaApod.Utilitatem
+{
+
+ internal static class Extensions
+    {
+        public static bool _IsDefined<T>(string name) where T : struct
+        {
+            return Enum.IsDefined(typeof(T), name);
+        }
+
+        public static bool _IsDefined<T>(T value) where T : struct
+        {
+            return Enum.IsDefined(typeof(T), value);
+        }
+
+        public static IEnumerable<T> _GetValues<T>() where T : struct
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+
+        public static T _GetDefault<T>() where T : struct
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>().FirstOrDefault();
+        }
+
+        public static bool _ContainsOrDefault<T>(this IEnumerable<T> list, T compare) where T : struct
+        {
+
+            if (list == null || !list.Any())
+                list = new List<T>() { _GetDefault<T>() };
+
+            return list.Any(x => x.Equals(compare) || x.Equals(_GetDefault<T>()));
+        }
+
+        public static string _GetDescription(this Enum value)
+        {
+            var attribute = value._GetAttribute<DescriptionAttribute>();
+
+            return attribute.Description;
+        }
+
+        public static T _GetAttribute<T>(this Enum value) where T : Attribute, new()
+        {
+            var type = value.GetType();
+
+            var memberInfo = type.GetMember(value.ToString());
+
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+
+            if (attributes == null || !attributes.Any())
+                return new T();
+
+            return (T)attributes[0];
+        }
+
+        public static T _ToEnum<T>(this string value) where T : struct
+        {
+            try
+            {
+                T enumValue;
+
+                if (!Enum.TryParse(value, true, out enumValue))
+                {
+                    return default(T);
+                }
+
+                return enumValue;
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+
+        public static T _ToEnum<T>(this string value, T defaultValue) where T : struct
+        {
+            try
+            {
+                T enumValue;
+
+                if (!Enum.TryParse(value, true, out enumValue))
+                {
+                    return defaultValue;
+                }
+
+                return enumValue;
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static string _EnumtoString<T>(this int value) where T : struct
+        {
+            try
+            {
+                var result = default(T);
+
+                bool success = Enum.IsDefined(typeof(T), value);
+
+                if (success)
+                {
+                    result = (T)Enum.ToObject(typeof(T), value);
+                }
+
+                return result.ToString();
+            }
+            catch (Exception)
+            {
+                return default(T).ToString();
+            }
+        }
+
+    }
+}
---
+++ Utilitatem/Enums.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace NasaApod.Utilitatem
+{
+
+    internal class Constants
+    {
+        public const string PRD = "Production";
+        public const string STG = "Stage";
+        public const string DEV = "Development";
+        public const string TST = "Test";
+        public const string NONE = "None";
+    }
+
+    public enum Scope
+    {
+        [Description(Constants.PRD)]
+        PRD = 0,
+        [Description(Constants.PRD)]
+        PROD = PRD,
+        [Description(Constants.PRD)]
+        PRODUCTION = PRD,
+
+        [Description(Constants.STG)]
+        STG = 1,
+        [Description(Constants.STG)]
+        UAT = STG,
+        [Description(Constants.STG)]
+        STAGE = STG,
+
+        [Description(Constants.DEV)]
+        DEV = 2,
+        [Description(Constants.DEV)]
+        DEVELOPMENT = DEV,
+
+        [Description(Constants.TST)]
+        TST = 3,
+        [Description(Constants.TST)]
+        TEST = TST,
+
+        [Description(Constants.NONE)]
+        NONE = 4,
+        [Description(Constants.NONE)]
+        DEFAULT = NONE,
+    }
+}
+
