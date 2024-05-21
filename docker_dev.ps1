96dad14b2f9ea5e94aeaadc538d5e49a9ac889d4
Bob McAllan <bobmcallan@gmail.com>
2024-05-22 09:19:04 +1000

    Updated

--- docker_dev.ps1
+++ docker_dev.ps1
@@ -1,3 +1,21 @@
-docker build -t caelumapi -f dockerfile .
+Push-Location (Split-Path $MyInvocation.MyCommand.Path)
 
-docker compose up -d
+try {
+
+    Write-Output "Pruning"
+    docker container prune -f
+    docker volume prune -f
+    docker image prune -f
+
+    Write-Output "Building"
+    docker build -t caelumapi -f dockerfile .
+
+    Write-Output "Compose Up"
+    docker compose up -d
+}
+catch {
+    Write-Host "An error occurred that could not be resolved."
+    Write-Host $_.ScriptStackTrace
+}
+
+Pop-Location
